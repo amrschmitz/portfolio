0fb6ea82c19292cd8965e37d8244606effca94aa
Alexandre Schmitz <amrschmitz@gmail.com>
2020-05-27 13:37:50 -0400

    filled in some content

--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import logo from "../images/carla.svg"
-
-const Card = ({ screenshot, screenshotAlt, logo, logoAlt, text1, text2, text3 }) => (
+const Card = ({ title, link, screenshot, screenshotAlt, logo, logoAlt, text1, text2, text3, text4 }) => (
   <div className="card">
-    <img src={screenshot} alt={screenshotAlt}/>
+    <a href={link} target="_blank" rel="noreferrer"><img src={screenshot} alt={screenshotAlt}/></a>
     <div className="content">
       <div className="top">
         <div className="line"></div>
         <img className="logo" src={logo} alt={logoAlt}/>
       </div>
-      <h2>inCORPUS</h2>
+      <h2>{title}</h2>
       <p className="text">
         {text1}<br/>
         {text2}<br/>
         {text3}<br/>
+        {text4}<br/>
       </p>
     </div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,30 +6,47 @@ import Card from "../components/card"
 
 import incorpus from "../images/inCORPUS.jpg"
 import incorpusLogo from "../images/inCORPUS-logo.svg"
+import maisonCedric from "../images/maison-cedric.jpg"
+import maisonCedricLogo from "../images/gatsby-logo.svg"
+import approximity from "../images/approximity.jpg"
+import approximityLogo from "../images/approximity-logo.svg"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Portfolio" />
-    <h1 className="main">Hi, I'm Alex. I'm a UX/UI designer and front-end developer from France & living in Montreal</h1>
+    <h1 className="main">Hi, I'm Alex. I'm a frontend developper and UX/UI designer from France living in Montreal</h1>
     <Card
+      title="inCORPUS®"
+      link="https://www.in-corpus.com/"
       screenshot={incorpus}
-      screenshotAlt='inCORPUS.jpg'
+      screenshotAlt="inCORPUS.jpg"
       logo={incorpusLogo}
-      logoAlt='inCORPUS-logo.svg'
-      left={true}
-      text1='Stack: GatsbyJS (React)'
-      text2='CMS: Contentful'
-      text3='APIs: ActiveCampaign, Intercom'
+      logoAlt="inCORPUS-logo.svg"
+      text1="Stack: GatsbyJS (React) - JS, HTML & CSS"
+      text2="Languages: French & English"
+      text3="CMS: Contentful"
+      text4="APIs: ActiveCampaign, Intercom"
     ></Card>
     <Card
-      screenshot={incorpus}
-      screenshotAlt='inCORPUS.jpg'
-      logo={incorpusLogo}
-      logoAlt='inCORPUS-logo.svg'
-      left={true}
-      text1='Stack: GatsbyJS (React)'
-      text2='CMS: Contentful'
-      text3='APIs: ActiveCampaign, Intercom'
+      title="Maison Cédric"
+      link="https://maisoncedric.netlify.app/"
+      screenshot={maisonCedric}
+      screenshotAlt="maison-cedric.jpg"
+      logo={maisonCedricLogo}
+      logoAlt="gatsby-logo.svg"
+      text1="Stack: GatsbyJS (React) - JS, HTML & CSS"
+      text2="Languages: French & English"
+      text3="CMS: Airtable"
+    ></Card>
+    <Card
+      title="Approximity"
+      link="https://approximity.herokuapp.com/"
+      screenshot={approximity}
+      screenshotAlt="approximity.jpg"
+      logo={approximityLogo}
+      logoAlt="approximity-logo.svg"
+      text1="Stack: Ruby on Rails - Ruby, JS, HTML & CSS"
+      text2="APIs: Mapbox, Google Maps for the seed"
     ></Card>
   </Layout>
 )
