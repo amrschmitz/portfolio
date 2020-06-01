ba1dfd3d329fe9539d3b0f19908eb46d6bb5bb6a
Alexandre Schmitz <amrschmitz@gmail.com>
2020-06-01 10:51:11 -0400

    changed the items of the card animations

--- src/components/card.js
+++ src/components/card.js
@@ -4,36 +4,55 @@ import Slide from 'react-reveal/Slide'
 
 const Card = ({ title, type, link, screenshot, screenshotAlt, logo, logoAlt, text1, text2, text3, text4, text5 }) => (
   <div className={type}>
-    { type === 'card right' &&
-      <Slide left>
-        <a href={link} target="_blank" rel="noreferrer"><img src={screenshot} alt={screenshotAlt}/></a>
-      </Slide>
-    }
-    { type === 'card left' &&
-      <Slide right>
-        <a href={link} target="_blank" rel="noreferrer"><img src={screenshot} alt={screenshotAlt}/></a>
-      </Slide>
-    }
+    <a href={link} target="_blank" rel="noreferrer"><img src={screenshot} alt={screenshotAlt}/></a>
     <div className="content">
       <div className="top">
         <div className="line"></div>
         <Fade><img className="logo" src={logo} alt={logoAlt}/></Fade>
       </div>
-      <a href={link} target="_blank" rel="noreferrer"><h2>{title}</h2></a>
-      { text1 &&
-        <p>{text1}</p>
+      { type === 'card left' &&
+        <Slide right>
+          <div>
+            <a href={link} target="_blank" rel="noreferrer"><h2>{title}</h2></a>
+            { text1 &&
+              <p>{text1}</p>
+            }
+            { text2 &&
+              <p>{text2}</p>
+            }
+            { text3 &&
+              <p>{text3}</p>
+            }
+            { text4 &&
+              <p>{text4}</p>
+            }
+            { text5 &&
+              <p>{text5}</p>
+            }
+          </div>
+        </Slide>
       }
-      { text2 &&
-        <p>{text2}</p>
-      }
-      { text3 &&
-        <p>{text3}</p>
-      }
-      { text4 &&
-        <p>{text4}</p>
-      }
-      { text5 &&
-        <p>{text5}</p>
+      { type === 'card right' &&
+        <Slide left>
+          <div>
+            <a href={link} target="_blank" rel="noreferrer"><h2>{title}</h2></a>
+            { text1 &&
+              <p>{text1}</p>
+            }
+            { text2 &&
+              <p>{text2}</p>
+            }
+            { text3 &&
+              <p>{text3}</p>
+            }
+            { text4 &&
+              <p>{text4}</p>
+            }
+            { text5 &&
+              <p>{text5}</p>
+            }
+          </div>
+        </Slide>
       }
     </div>
   </div>
