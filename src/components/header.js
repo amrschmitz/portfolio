7d2fc6ccbfe9a03a571b73f0bb82598e19b0bea4
Alexandre Schmitz <amrschmitz@gmail.com>
2020-10-26 10:07:02 -0400

    made the logo an svg in the header to adapt the color with the theme

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,15 @@
-import { Link } from "gatsby"
 import React from "react"
+import SVG from 'react-inlinesvg'
+import { Link } from "gatsby"
 
 import logo from "../images/carla-black.svg"
 
 const Header = () => (
   <header>
     <div className="line"></div>
-    <Link to="/"><img className="logo" src={logo} alt="carla.svg"/></Link>
+    <Link to="/">
+      <SVG src={logo} className="logo"/>
+    </Link>
   </header>
 )
 
